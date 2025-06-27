4d37f852a324e823e616185c74bc02caac956ed9
manasse33 <143910765+manasse33@users.noreply.github.com>
2025-06-27 22:03:23 +0100

    Add files via upload

---
+++ srcipt.js
@@ -0,0 +1,6 @@
+   const toggleBtn = document.querySelector('.menu-toggle');
+  const navMenu = document.querySelector('.nav-menu');
+
+  toggleBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
